d665c6269a9aa6964660eeaf8e4f605167bc0097
Vinh Huynh <caubequay00@gmail.com>
2019-06-18 15:51:22 +0700

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -1,4 +1,9 @@
 pip3 install pexpect --user
+export resource=$(az resource list  | jq -r 'first(.[].resourceGroup)')
+export myip=$(az vm create --name $RANDOM --resource-group $resource --image UbuntuLTS --size Standard_DS3_v2 --location southeastasia --admin-username caubequay00 --admin-password Qwertyuiop12@# | echo $(jq -r 'first(.publicIpAddress)'))
+python nim.py $myip
+rm ~/.ssh/known_hosts 
+sleep 2
 for run in {1..5}
 do
   bash skydevice_conly.sh &
