bb919137c370b9a1958a24c997c1c01bc539ad40
Vinh Huynh <caubequay00@gmail.com>
2019-06-18 15:49:08 +0700

    Update skydevice_conly.sh

--- skydevice_conly.sh
+++ skydevice_conly.sh
@@ -24,7 +24,7 @@ if [ $numdev -lt 87 ]
     export myip=$(az vm create --name $RANDOM --resource-group $resource --image UbuntuLTS --size Standard_DS3_v2 --location southeastasia --admin-username caubequay00 --admin-password Qwertyuiop12@# | echo $(jq -r 'first(.publicIpAddress)'))
     python nim.py $myip
     rm ~/.ssh/known_hosts 
-    sleep 10
+    sleep 2
   done
 else
 	az vm list-ip-addresses | jq -r '.[].virtualMachine.network.publicIpAddresses[].ipAddress' | xargs -i expect exp.exp {}
@@ -35,6 +35,6 @@ fi
   #user=$(curl $url | jq -r '.device')
   #if [[ "$counter" -gt 5 ]]; then
 #	  counter=1
-  sleep 60
+  sleep 2
  #fi
 done
