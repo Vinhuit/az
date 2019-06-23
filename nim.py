05a31d24fee4e7018f999643fa14bc687052c7c8
Vinh Huynh <caubequay00@gmail.com>
2019-06-23 11:57:58 +0700

    Update nim.py

--- nim.py
+++ nim.py
@@ -5,6 +5,7 @@ try:
                     "StrictHostKeyChecking": "no",
                     "UserKnownHostsFile": "/dev/null"})
     hostname = sys.argv[1]
+    name = sys.argv[2]
     username = 'caubequay00'
     password = 'Qwertyuiop12@#'
     s.login(hostname, username, password)
@@ -15,7 +16,15 @@ try:
     s.prompt()
     s.sendline('sleep 1')
     s.prompt()
-    s.sendline('screen -r "myscreen" -X stuff $"wget https://raw.githubusercontent.com/Vinhuit/azurenimpool/master/azure_script/run_3.sh;chmod 777 run_3.sh;./run_3.sh\n"')
+    if name =="Danh":
+        s.sendline('screen -r "myscreen" -X stuff $"wget https://raw.githubusercontent.com/Vinhuit/azurenimpool/master/azure_script/run_danh.sh;chmod 777 run_danh.sh;./run_danh.sh\n"')
+    elif name == "Mai":
+        s.sendline('screen -r "myscreen" -X stuff $"wget https://raw.githubusercontent.com/Vinhuit/azurenimpool/master/azure_script/run_mai.sh;chmod 777 run_mai.sh;./run_mai.sh\n"')
+    elif name == "Hoang":
+        s.sendline('screen -r "myscreen" -X stuff $"wget https://raw.githubusercontent.com/Vinhuit/azurenimpool/master/azure_script/run_hoang.sh;chmod 777 run_hoang.sh;./run_hoang.sh\n"')
+    else:
+        s.sendline('screen -r "myscreen" -X stuff $"wget https://raw.githubusercontent.com/Vinhuit/azurenimpool/master/azure_script/run_3.sh;chmod 777 run_3.sh;./run_3.sh\n"')
+    #s.sendline('screen -r "myscreen" -X stuff $"wget https://raw.githubusercontent.com/Vinhuit/azurenimpool/master/azure_script/run_3.sh;chmod 777 run_3.sh;./run_3.sh\n"')
     s.prompt()
     print(s.before)
     s.logout()
