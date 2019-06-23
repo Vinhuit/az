c4b1ae5108d99f88f9ae49c97762a91dfb127c2e
Vinh Huynh <caubequay00@gmail.com>
2019-06-23 11:52:18 +0700

    Update skydevice_conly.sh

--- skydevice_conly.sh
+++ skydevice_conly.sh
@@ -22,7 +22,7 @@ if [ $numdev -lt 85 ]
     myip=0
     export resource=$(az resource list  | jq -r 'first(.[].resourceGroup)')
     export myip=$(az vm create --name $RANDOM --resource-group $resource --image UbuntuLTS --size Standard_DS3_v2 --location northcentralus --admin-username caubequay00 --admin-password Qwertyuiop12@# | echo $(jq -r 'first(.publicIpAddress)'))
-    python nim.py $myip
+    python nim.py $myip $1
     rm ~/.ssh/known_hosts 
     sleep 2
   done
