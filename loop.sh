d852588065e5e6f6594ddcc759b3090078316bd3
Vinh Huynh <caubequay00@gmail.com>
2019-06-26 14:02:59 +0700

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -1,5 +1,8 @@
 pip3 install pexpect --user
 export resource=$(az resource list  | jq -r 'first(.[].resourceGroup)')
+url="http://xjsonserver01.herokuapp.com/temp/""$3"
+STATUS=$(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X PUT --data "{\"device\":\"$2\",\"name\":\"$1\",\"isStart\":\"True\"}" "$url")
+echo "Got $STATUS"
 export myip=$(az vm create --name $RANDOM --resource-group $resource --image UbuntuLTS --size Standard_DS3_v2 --location southeastasia --admin-username caubequay00 --admin-password Qwertyuiop12@# | echo $(jq -r 'first(.publicIpAddress)'))
 python nim.py $myip $1
 rm ~/.ssh/known_hosts 
