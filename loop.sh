3af998d8c8ef9a4f45015050407c939ad8fc4090
Vinh Huynh <caubequay00@gmail.com>
2019-07-08 16:57:51 +0700

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -4,6 +4,7 @@ if [ $3 -gt 499 ]; then
   url="http://xjsonserver01.herokuapp.com/temp/""$3"
 else
   url="http://xjsonserver01.herokuapp.com/rerunaccount/""$3"
+fi
 STATUS=$(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X PUT --data "{\"device\":\"$2\",\"name\":\"$1\",\"isStart\":\"True\"}" "$url")
 echo "Got $STATUS"
 export myip=$(az vm create --name $RANDOM --resource-group $resource --image UbuntuLTS --size Standard_DS3_v2 --location southeastasia --admin-username caubequay00 --admin-password Qwertyuiop12@# | echo $(jq -r 'first(.publicIpAddress)'))
