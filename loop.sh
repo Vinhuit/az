8049af8453eb9a01e400d76613c0819697b9b532
Vinh Huynh <caubequay00@gmail.com>
2019-07-12 17:48:51 +0700

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -7,11 +7,11 @@ else
 fi
 STATUS=$(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X PUT --data "{\"device\":\"$2\",\"name\":\"$1\",\"isStart\":\"True\"}" "$url")
 echo "Got $STATUS"
-export myip=$(az vm create --name $RANDOM --resource-group $resource --image UbuntuLTS --size Standard_DS3_v2 --location southeastasia --admin-username caubequay00 --admin-password Qwertyuiop12@# | echo $(jq -r 'first(.publicIpAddress)'))
-python nim.py $myip $1
+#export myip=$(az vm create --name $RANDOM --resource-group $resource --image UbuntuLTS --size Standard_DS3_v2 --location southeastasia --admin-username caubequay00 --admin-password Qwertyuiop12@# | echo $(jq -r 'first(.publicIpAddress)'))
+#python nim.py $myip $1
 rm ~/.ssh/known_hosts 
 sleep 2
-for run in {1..20}
+for run in {1..50}
 do
   bash skydevice_conly.sh $1 &
   sleep 2
