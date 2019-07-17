40cd45b6e52cccc3b1825a718008208d88b01b68
Vinh Huynh <caubequay00@gmail.com>
2019-07-17 16:06:57 +0700

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -10,10 +10,13 @@ echo "Got $STATUS"
 #export myip=$(az vm create --name $RANDOM --resource-group $resource --image UbuntuLTS --size Standard_DS3_v2 --location southeastasia --admin-username caubequay00 --admin-password Qwertyuiop12@# | echo $(jq -r 'first(.publicIpAddress)'))
 #python nim.py $myip $1
 #export resource=$(az resource list  | jq -r 'first(.[].resourceGroup)')
+numx=0
 for run in {1..20}
 do
   for i in `seq 1 5` ;do az container create   --resource-group $resource   --name mycontainer$(head /dev/urandom | tr -dc a-z| head -c 5)   --image caubequay00/az-hk-cmd --ports 80   --dns-name-label $(head /dev/urandom | tr -dc a-z| head -c 5)$DNS_NAME_LABEL   --location eastus --cpu 4 --memory 16 --command-line "bash run_3.sh" & done
   sleep 60
+  numx=numx+1
+  echo $numx
 done
 #rm ~/.ssh/known_hosts 
 #sleep 2
