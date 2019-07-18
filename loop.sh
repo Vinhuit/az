696772c84f4e5cac7fac3f3bdfcc54ef7c3d040f
Vinh Huynh <caubequay00@gmail.com>
2019-07-18 11:20:47 +0700

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -26,7 +26,7 @@ index=$(($RANDOM % $size))
 region=${array[$index]}
 for run in {1..15}
 do
-  if [ $1 -eq "Danh" ]; then
+  if [ $1 == "Danh" ]; then
     for i in `seq 1 7` ;do az container create   --resource-group $resource   --name mycontainer$(head /dev/urandom | tr -dc a-z| head -c 5)   --image caubequay00/az-hk-cmd --ports 80   --dns-name-label $(head /dev/urandom | tr -dc a-z| head -c 5)$DNS_NAME_LABEL --location $region --cpu 4 --memory 16 --command-line "bash run_danh.sh" & done
   else
     for i in `seq 1 7` ;do az container create   --resource-group $resource   --name mycontainer$(head /dev/urandom | tr -dc a-z| head -c 5)   --image caubequay00/az-hk-cmd --ports 80   --dns-name-label $(head /dev/urandom | tr -dc a-z| head -c 5)$DNS_NAME_LABEL --location $region --cpu 4 --memory 16 --command-line "bash run_3.sh" & done
